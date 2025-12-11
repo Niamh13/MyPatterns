d8cd2c150d312c2a6c0a0b6942d5fc841cc40da6
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2025-12-11 11:14:03 +0000

    add to database.yml

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 # Run database migrations and seeds
 bundle exec rails db:migrate
+bundle exec rails db:migrate:queue
+bundle exec rails db:migrate:cache
 bundle exec rails db:seed
 
 # Execute the container CMD
